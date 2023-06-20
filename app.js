7abbb08fbfba53fb91dc4276eefc0c5f8c1fedec
Nasih <iamnasih@gmail.com>
2023-06-20 18:30:49 +0530

    dotenv added

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express()
+const port  = 5000 || process.env.port;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+// static files
+app.use(express.static('public'));
