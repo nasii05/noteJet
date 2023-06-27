8f3f9f0393edd375b4603788befec69cd8b37129
Nasih <iamnasih@gmail.com>
2023-06-28 01:06:26 +0530

    signup bug fixed

--- app.js
+++ app.js
@@ -2,16 +2,33 @@ require('dotenv').config()
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const connectDB = require('./server/config/db')
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const mongoStore = require('connect-mongo');
+
  
 const app = express()
 const port  = 5000 || process.env.port;
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+    })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 // connect to database
 connectDB();
+
+
 
 // static files
 app.use(express.static('public'));
@@ -23,6 +40,7 @@ app.set('layout','./layouts/main');
 app.set('view engine', 'ejs');
 
 // Routes 
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
---
+++ server/routes/auth.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value,
+      };
+
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
+
+// google Login Route
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+// Retrieve user data
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login-failure",
+    successRedirect: "/dashboard",
+  })
+);
+
+// Route if something goes wrong
+router.get("/login-failure", (req, res) => {
+  res.send("Something went wrong...");
+});
+
+// destroy user session
+router.get('/logout', (req, res)=>{
+  req.session.destroy(error =>{
+    if(error){
+      console.log(error);
+      res.send("error logging out")
+    }else{
+      res.redirect('/')
+    }
+  })
+})
+
+// Presist user data after succesfil authentication
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// Retrieve user data from sesson
+passport.deserializeUser(async(id, done)=>{
+   try{
+    const user = await User.findById(id);
+    done(null, user)
+   }catch(error){
+    done(error, null)
+   }
+})
+
+module.exports = router;
